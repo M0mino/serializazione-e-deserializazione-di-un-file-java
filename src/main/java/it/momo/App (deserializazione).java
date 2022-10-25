f5edd2cb28565a1bd0b2af98b4e11df45a649c9d
lai2-01 <lai2-01@itismeucci.com>
2022-10-25 10:09:38 +0200

    rinomina delle classi appper capire chi fa cosa

---
+++ src/main/java/it/momo/App (deserializazione).java
@@ -0,0 +1,39 @@
+package it.momo;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.Date;
+
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws IOException
+    {
+
+        classe momo = new classe();
+        alunno a1= new alunno("momo","momino",new Date(2003, 12, 24));
+        alunno a2= new alunno("bob","bianchi",new Date(2004, 5, 16));
+        alunno a3= new alunno("marco","rossi",new Date(2005, 2, 5));
+        alunno a4= new alunno("gianni","verdi",new Date(3030, 7, 20));
+
+        momo.LAlunni.add(a1);
+        momo.LAlunni.add(a2);
+        momo.LAlunni.add(a3);
+        momo.LAlunni.add(a4);
+
+
+       
+        File file = new File("Classe.xml");
+    XmlMapper xmlMapper = new XmlMapper();
+    classe value = xmlMapper.readValue(file, classe.class);
+    System.out.println(value.getGrado() == 5 && value.getSezione().equals("B") && value.getPosizione().equals("TC-09"));
+
+
+    }
+
+}
